67c541a2724aa37eab8f9ff634deb61d8bc09e7a
Marin Vladimir <mvladi00@fesb.hr>
2022-12-12 10:37:56 +0100

    fixing tests

--- src/components/tests/DropDown.test.jsx
+++ src/components/tests/DropDown.test.jsx
@@ -1,26 +1,66 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import DropDown from "../DropDown";
 import { Item } from "../../data/data";
+import { ThemeProvider } from "styled-components";
+import {
+  primaryDesign,
+  quaternaryDesign,
+  secondaryDesign,
+  tertiaryDesign,
+} from "../../styles/themes";
+import { act } from "react-dom/test-utils";
+
+const getTheme = (col) => {
+  switch (col) {
+    case "primary":
+      return primaryDesign;
+    case "secondary":
+      return secondaryDesign;
+    case "tertiary":
+      return tertiaryDesign;
+    case "quaternary":
+      return quaternaryDesign;
+    default:
+      return primaryDesign;
+  }
+};
+
+const setSelectedItem = jest.fn();
+
+afterEach(() => {
+  setSelectedItem.mockClear();
+});
 
 describe("dropdown component tests", () => {
   test("renders the DropDown component", () => {
-    render(<DropDown options={Item} />);
+    render(
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown options={Item} />
+      </ThemeProvider>
+    );
   });
 
   test("expects the select button in the component", () => {
-    const { getByTestId } = render(<DropDown options={Item} />);
+    const { getByTestId } = render(
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown options={Item} />
+      </ThemeProvider>
+    );
     const btn = getByTestId("button");
     expect(btn).toBeInTheDocument();
   });
 
   test("expects the dropdown to show in the component after button click ", async () => {
     const baseDom = render(
-      <DropDown
-        options={Item}
-        selectedItem={"null"}
-        setSelectedItem={() => {}}
-      />
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={"null"}
+          setSelectedItem={() => {}}
+        />
+      </ThemeProvider>
     );
     fireEvent.click(await baseDom.findByTestId("button"));
     expect(baseDom.queryAllByTestId("dropdown_option")[0]).toBeTruthy();
@@ -28,11 +68,13 @@ describe("dropdown component tests", () => {
 
   test("expects the dropdown to close after keydown ESCAPE", async () => {
     const baseDom = render(
-      <DropDown
-        options={Item}
-        selectedItem={"null"}
-        setSelectedItem={() => {}}
-      />
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={"null"}
+          setSelectedItem={() => {}}
+        />
+      </ThemeProvider>
     );
     fireEvent.click(await baseDom.findByTestId("button"));
     expect(baseDom.queryAllByTestId("dropdown_option")[0]).toBeTruthy();
@@ -51,25 +93,70 @@ describe("dropdown component tests", () => {
 
   test("expects the selected item to have default text on render", () => {
     const baseDom = render(
-      <DropDown
-        options={Item}
-        selectedItem={"null"}
-        setSelectedItem={() => {}}
-      />
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={"null"}
+          setSelectedItem={() => {}}
+        />
+      </ThemeProvider>
     );
     expect(screen.getByText("Filter title")).toBeInTheDocument();
   });
 
   test("expects the selected item to show in the DOM when it exists", async () => {
     const baseDom = render(
-      <DropDown
-        options={Item}
-        selectedItem={Item[0]}
-        setSelectedItem={() => {}}
-      />
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={Item[0]}
+          setSelectedItem={setSelectedItem}
+        />
+      </ThemeProvider>
     );
     expect(baseDom.findByTestId("select_text")).toBeTruthy();
     expect(screen.queryAllByText("Filter title")).toHaveLength(0);
     expect(screen.queryAllByText(Item[0].name)).toHaveLength(1);
+  });
+
+  test("expects the dropdown not to close on list element click", async () => {
+    const baseDom = render(
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={null}
+          setSelectedItem={setSelectedItem}
+        />
+      </ThemeProvider>
+    );
+    const btn = await baseDom.findByTestId("button");
+    act(() => {
+      userEvent.click(btn);
+    });
+    expect(baseDom.queryAllByTestId("dropdown_option")[0]).toBeTruthy();
+
+    const itemFromList = await baseDom.queryAllByTestId("dropdown_option")[0];
+    act(() => {
+      userEvent.click(itemFromList);
+    });
+
+    expect(setSelectedItem).toHaveBeenCalledTimes(1);
+
+    expect(baseDom.queryAllByTestId("dropdown_option")[0]).toBeTruthy();
+  });
+
+  test("expects the dropdown to close on doubleclick", async () => {
+    const baseDom = render(
+      <ThemeProvider theme={getTheme("primary")}>
+        <DropDown
+          options={Item}
+          selectedItem={"null"}
+          setSelectedItem={() => {}}
+        />
+      </ThemeProvider>
+    );
+    fireEvent.click(await baseDom.findByTestId("button"));
+    fireEvent.click(await baseDom.findByTestId("button"));
+    expect(baseDom.queryAllByTestId("dropdown_option")).toHaveLength(0);
   });
 });
